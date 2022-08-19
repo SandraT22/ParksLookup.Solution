974ccaad9e6f912eaecb3ae74a0f7b55c70d12ce
Sandra Tena <sandratena06@gmail.com>
2022-08-19 10:15:03 -0700

    add models

---
+++ ParksLookup/Models/Park.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace ParksLookup.Solution
+{
+    public class Park
+    {
+        public string Name { get; set; }
+
+        public string Designation { get; set; }
+
+        public string Region { get; set; }
+
+        public string State { get; set; }
+    }
+}
---
+++ ParksLookup/Models/ParkLookupContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ParksLookup.Models
+{
+    public class ParksLookupContext : DbContext
+    {
+        public ParksLookupContext(DbContextOptions<ParksLookupContext> options)
+            : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+{
+  builder.Entity<Park>()
+      .HasData(
+          new Park { ParkId = 1, Name = "Glacier", Designation = "National Park", Region = "Northwestern", State = "Montana" },
+          new Park { ParkId = 2, Name = "Cape Lookout", Designation = "State Park", Region = "Northern Coast", State = "Oregon" },
+          new Park { ParkId = 3, Name = "Olympic", Designation = "National Park", Region = "Olympic Peninsula", State = "Washington" }
+);
+}
+        public DbSet<Park> Parks { get; set; }
+    }
+}
